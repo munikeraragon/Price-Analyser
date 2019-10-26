c9eb167b2245089b1d17a2b6665d2ce86d0bf21b
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-10-25 17:26:02 -0700

    added work

--- webapp/userApp/views.py
+++ webapp/userApp/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 # Create your views here.
 def login(request):
     return render(request,'userApp/login.html')
 def register(request):
-    return render(request,'userApp/register.html')
+    if(request.method == 'POST'):
+        form = UserRegisterForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request,'userApp/register.html', {'form':form})
