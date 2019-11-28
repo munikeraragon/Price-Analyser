8ad73233334ad8f21d8c67103d7d0d1b733dfe65
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-11-28 10:49:00 -0800

    Developing WebScraper

--- webapp/userApp/webScrape.py
+++ webapp/userApp/webScrape.py
@@ -49,3 +49,16 @@ def amazon(URL):
     data = e.extract(r.text)
     # Print the data 
     print(json.dumps(data, indent=True))
+
+def walmart(URL):
+    headers = {'user-agent': 'Mozilla/5.0 (Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0',}
+    #url = "https://www.bestbuy.com/site/google-pixel-4-with-64gb-cell-phone-unlocked-just-black/6382490.p?skuId=6382490"
+    #url = "https://www.bestbuy.com/site/microsoft-xbox-one-s-1tb-nba-2k20-bundle-white/6350265.p?skuId=6350265"
+    url = URL
+    # store page html
+    htmlPage = requests.get(url, headers=headers)
+    # store in beatiful soup format
+    soup = BeautifulSoup(htmlPage.text, "html.parser")
+    priceContainer = soup.find("div", {"class":"prod-PriceHero"})
+    price = priceContainer.find("span",{"class":"visuallyhidden"}).text
+    print(price)
