6e900a3c5a0272579ea9ee3c40d60a3d07c4ef86
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-12-05 19:20:54 -0800

    Fixed Amazon connection issue

--- app/webapp/userApp/webScrape.py
+++ app/webapp/userApp/webScrape.py
@@ -42,7 +42,7 @@ def bestBuy(URL):
 def amazon(URL):
     try:
         # Create an Extractor by reading from the YAML file
-        e = Extractor.from_yaml_file('selectors.yml')
+        e = Extractor.from_yaml_file('C:\\Users\\santi\\Desktop\\CS320 project\\Dajngo_webapp\\app\\webapp\\userApp\\selectors.yml')
 
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
         headers = {'User-Agent': user_agent}
