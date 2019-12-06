3cb02c67208daf9cc4422636f881c8b064c45e12
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-12-05 16:19:05 -0800

    _

--- app/webapp/userApp/views.py
+++ app/webapp/userApp/views.py
@@ -19,6 +19,33 @@ def register(request):
 def homelog(request):
     if(request.method == 'POST'):
         form = ItemForm(request.POST)
+        price = False
+        if(form.is_valid()):
+            item = form.save(commit=False)
+            item.currentUser = request.user
+            data = form.cleaned_data
+            url = data['itemURL']
+            store = data['store']
+            if(store == "BestBuy"):
+                price = bestBuy(url)
+            elif(store == "Amazon"):
+                price = amazon(url)
+            elif(store == "Walmart"):
+                price = walmart(url)
+        if(price != False):
+            item.itemPrice = price
+            item.save()
+            return redirect('myItems')
+    else:
+        form = ItemForm()
+    return render(request,'userApp/homelog.html',{'form':form})
+def myItems(request):
+    userItems = Item.objects.filter(currentUser=request.user)
+    return render(request,'userApp/myItems.html', {'userItems':userItems})
+
+'''def homelog(request):
+    if(request.method == 'POST'):
+        form = ItemForm(request.POST)
         if(form.is_valid()):
             item = form.save(commit=False)
             item.currentUser = request.user
@@ -29,11 +56,9 @@ def homelog(request):
             if(price != False):
                 item.itemPrice = price
                 item.save()
-                return redirect('homelog')
+            return redirect('myItems')
     else:
         form = ItemForm()
     return render(request,'userApp/homelog.html',{'form':form})
-def myItems(request):
-    userItems = Item.objects.filter(currentUser=request.user)
-    return render(request,'userApp/myItems.html', {'userItems':userItems})
 
+'''    
