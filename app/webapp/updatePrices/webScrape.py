e9261f82e6e05b218e56e4a55de638735ed3d048
munikeraragon <54874076+munikeraragon@users.noreply.github.com>
2019-12-16 09:29:46 -0800

    relocated webScraing files

--- app/webapp/updatePrices/webScrape.py
+++ app/webapp/updatePrices/webScrape.py
@@ -41,44 +41,26 @@ def bestBuy(URL):
         return False
     
 
-'''def amazon(URL):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',}
-    #url = "https://www.bestbuy.com/site/google-pixel-4-with-64gb-cell-phone-unlocked-just-black/6382490.p?skuId=6382490"
-    #url = "https://www.bestbuy.com/site/microsoft-xbox-one-s-1tb-nba-2k20-bundle-white/6350265.p?skuId=6350265"
-    url = URL
-    # store page html
-    htmlPage = requests.get(url, headers=headers)
-    # store in beatiful soup format
-    soup = BeautifulSoup(htmlPage.text, "html.parser")
-    print(soup)
-    productPrice = soup.find("div", {"class":"a-section a-spacing-small"})
-    print(productPrice)
-'''
+
 def amazon(URL):
     try:
         #get proxy ip
         proxies = get_proxies()
         proxy_pool = cycle(proxies)
-
         userAgent_pool = iter(userAgentList)
-
         # Create an Extractor by reading from the YAML file
         e = Extractor.from_yaml_file("C:/Users/santi/Desktop/CS320 project/Dajngo_webapp/app/webapp/userApp/selectors.yml")
         #santiago path = C:\\Users\\santi\\Desktop\\CS320 project\\Dajngo_webapp\\app\\webapp\\userApp\\selectors.yml
         #Trever's path = /home/treverhibbs/Documents/projects/Price-Analyser/app/webapp/userApp/selectors.yml
-
         for i in range(1,4):
             #get proxy from pool
             proxy = next(proxy_pool)
-
             #get user agent from user agent pool
             user_agent = next(userAgent_pool)
             print(user_agent)
             print(proxy)
             print("Request #%d"%i)
-
             headers = {'http': proxy, 'https': proxy, 'User-Agent': user_agent}
-
             try:
                 # Download the page using requests
                 r = requests.get(URL, headers=headers)
@@ -88,16 +70,7 @@ def amazon(URL):
                 #skip if no connect
                 print("Skipping. Connnection error")
 
-    
         data = e.extract(r.text)
-
-        #print the data to a file
-        #filePointer = open('AmazonWebPage.html', 'w')
-        #print(r.text, file = filePointer)
-        #filePointer.close()
-
-        # Print the data 
-        #print(json.dumps(data, indent=True))
         return(data["sale_price"])
     except ValueError:
         return False
--- app/webapp/userApp/views.py
+++ app/webapp/userApp/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, ItemForm
 from django.contrib.auth.models import User
 from .models import Item
-from .webScrape import *
+from updatePrices.webScrape import *
 
-# Create your views here.
+
 def register(request):
     if(request.method == 'POST'):
         form = UserRegisterForm(request.POST)
@@ -43,23 +43,4 @@ def homelog(request):
 def myItems(request):
     userItems = Item.objects.filter(currentUser=request.user)
     return render(request,'userApp/myItems.html', {'userItems':userItems})
-
-'''def homelog(request):
-    if(request.method == 'POST'):
-        form = ItemForm(request.POST)
-        if(form.is_valid()):
-            item = form.save(commit=False)
-            item.currentUser = request.user
-            data = form.cleaned_data
-            url = data['itemURL']
-            store = data['store']
-            price = bestBuy(url)
-            if(price != False):
-                item.itemPrice = price
-                item.save()
-            return redirect('myItems')
-    else:
-        form = ItemForm()
-    return render(request,'userApp/homelog.html',{'form':form})
-
-'''    
+    
